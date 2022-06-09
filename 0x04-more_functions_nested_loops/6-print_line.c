04ad723e5f6c62a26142b95147871c58e3a0baf7
Meskour <sotradia@gmail.com>
2022-06-09 16:55:26 -0700

    print lines _

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line ..
+ *
+ * @n: input n nomber of times of _
+ *
+ * Retrun: void
+*/
+
+void print_line(int n)
+{
+	int i = 1;
+
+	while (n <= n)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('_');
+		++i;
+	}
+	_putchar('$');
+	_putchar('\n');
+}
