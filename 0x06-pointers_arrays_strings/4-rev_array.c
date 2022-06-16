29c475a19e4b3230aaa7d4fd2f9b218cee32eac4
Meskour <sotradia@gmail.com>
2022-06-16 15:42:40 -0700

    reverse the content of an array of int..

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,12 @@
 void reverse_array(int *a, int n)
 {
 	int temp;
-	int s = 0;
-	int e = n -1;
 
-	while ( s < e )
+	int s = 0;
+
+	int e = n - 1;
+
+	while (s < e)
 	{
 		temp = a[s];
 		a[s] = a[e];
