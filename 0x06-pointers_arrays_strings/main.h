c70f5f11cccb079ae3084e7c8fbd55294377a6ef
Meskour <sotradia@gmail.com>
2022-06-16 18:58:20 -0700

    prints an integer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ void reverse_array(int *a, int n);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-
+void print_number(int n);
+int _putchar(char c);
 #endif /* end main.h */
