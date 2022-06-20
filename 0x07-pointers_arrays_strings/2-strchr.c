38b55599256e354eea0c1a8cac277eb442003cb8
Meskour <sotradia@gmail.com>
2022-06-20 10:03:58 -0700

    Locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,14 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
 	for (i = 0; *s != '\0'; i++)
-	{
 		if (s[i] == c)
 			return s + i;
-	}
+
+	if (s[i] == c)
+		return s + i;
 
 	return ('\0');
 }
