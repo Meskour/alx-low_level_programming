1d788fedcb22a340d09cd1aab3e86e430cb915a7
Meskour <sotradia@gmail.com>
2022-07-07 15:24:25 -0700

    returns the sum of all its parameters.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,12 +11,12 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ap;
 	unsigned int i, sum;
 
-	va_start (ap, n);         /* Initialize the argument list. */
+	va_start(ap, n);         /* Initialize the argument list. */
 
 	sum = 0;
 	for (i = 0; i < n; i++)
-		sum += va_arg (ap, int);    /* Get the next argument value. */
+		sum += va_arg(ap, int);    /* Get the next argument value. */
 
-	va_end (ap);                  /* Clean up. */
+	va_end(ap);                  /* Clean up. */
 	return (sum);
 }
